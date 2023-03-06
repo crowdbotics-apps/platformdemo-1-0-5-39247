8976a621121201184e89f6aca3c83bb59a4010d6
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-03-07 00:03:48 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,28 @@
+import { ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      backgroundColor: '#E45700',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><ImageBackground style={styles.YtGPYkEL} source={require("./Recipeful(2).png")} resizeMode="cover"></ImageBackground></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  YtGPYkEL: {
+    width: 336,
+    height: 195,
+    position: "absolute",
+    top: 110,
+    left: 10.5
   }
 });
 export default Untitled1;
--- store/upfood/api.js
+++ store/upfood/api.js
@@ -1,9 +1,20 @@
-import axios from "axios"
+import axios from "axios";
 const upfood = axios.create({
   baseURL: "https://app.ecwid.com/api/v3/39042093",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function upfood_get_orders_read(payload) {
-  return upfood.get(`/orders`, { params: { token: payload.token } })
+  return upfood.get(`/orders`, {
+    params: {
+      token: payload.token
+    }
+  });
 }
-export const apiService = { upfood_get_orders_read }
+
+export const apiService = {
+  upfood_get_orders_read
+};
